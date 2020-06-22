c8028410dee083672cf5ef2e2ba991b478a1b4e4
sztuczkap <p.sztuczka@wp.pl>
2020-06-22 21:11:16 +0200

    adding files

---
+++ pet-clinic-data/src/test/java/sztuczkap/springframework/petclinicdata/PetClinicDataApplicationTests.java
@@ -0,0 +1,13 @@
+package sztuczkap.springframework.petclinicdata;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
