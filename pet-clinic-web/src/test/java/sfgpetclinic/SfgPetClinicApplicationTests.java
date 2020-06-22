f6ea89f0722ca9e7e6c9205059734c95b5e80708
sztuczkap <p.sztuczka@wp.pl>
2020-06-22 19:31:37 +0200

    refactoring to multimodule project. closes #1

--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,6 +1,7 @@
-package sztuczkap.springframework.sfgpetclinic;
+package sfgpetclinic;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
